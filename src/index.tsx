6a4a4afcd75c6b3b326673988cdf32fbc1291236
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-12-02 14:23:54 +0100

    create: add upgrade React-18 (ReactDOM.createRoot to createRoot)

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,22 @@ import './index.css';
 import App from './App';
 
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(
+//   document.getElementById('root') as HTMLElement
+// );
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+import { createRoot } from 'react-dom/client';
+
+const container = document.getElementById('root');
+const root = createRoot(container!); // createRoot(container!) if you use TypeScript
+root.render(<App  />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 
+// git remote add origin git@github.com:AleksanderZabrodski09/MY-TODO-2.git
