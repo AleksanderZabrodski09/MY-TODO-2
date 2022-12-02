756ceda5ac3e9de924bb162c8ce0b1d837bb845a
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-12-02 16:01:58 +0100

    create: add method map for tasks and remove 2d Todolist (2)

--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 
 
-
-type TaskType ={
+type TaskType = {
   id: number
   title: string
   isDone: boolean
 }
 type TodolistType = {
-title:string
-  tasks:TaskType[]
+  title: string
+  tasks: TaskType[]
 }
 export const Todolist: React.FC<TodolistType> = (props) => {
   return <div>
@@ -19,9 +18,17 @@ export const Todolist: React.FC<TodolistType> = (props) => {
       <button>+</button>
     </div>
     <ul>
-      <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
-      <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
-      <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+      {
+        props.tasks.map(t=>{
+          // debugger
+          return <li key={t.id}>
+            <input type="checkbox" checked={t.isDone}/> <span>{t.title}</span>
+          </li>
+        })
+      }
+      {/*<li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>*/}
+      {/*<li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>*/}
+      {/*<li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>*/}
     </ul>
     <div>
       <button>All</button>
