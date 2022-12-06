dcb0491300a96a58cf6218a3b18ba7161787c8e3
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-12-06 17:07:29 +0100

    create: add useState for Tasks and fix  removeTasks (1)

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,14 @@ function App() {
   // const [tasks, setTasks]=useState([
   // ])
 
-  const tasks = [
+  const [tasks, setTasks] =useState([
     {id: 1, title: "HTML&CSS", isDone: true},
     {id: 2, title: "JS", isDone: true},
     {id: 3, title: "ReactJS", isDone: false}
-  ]
+  ])
 
   const removeTask=(taskId:number)=>{
-    tasks.filter(t=>t.id!==taskId)
+    setTasks( tasks.filter(t=>t.id!==taskId))
     console.log('yo')
   }
 
