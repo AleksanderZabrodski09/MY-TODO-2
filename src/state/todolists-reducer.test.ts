11d5b0c4d3e9690d4e85334d3ed9b337d85a41d2
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-12-11 15:43:44 +0100

    create:add refactoring todolists-reducer.test.ts  (8)

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -1,23 +1,41 @@
 import {TodolistType} from '../App';
 import {v1} from 'uuid';
-import {addTodolistAC, todolistsReducer} from './todolists-reducer';
+import {addTodolistAC, removeTodolistAC, todolistsReducer} from './todolists-reducer';
 
+const todolistId1 = v1()
+const todolistId2 = v1()
+let startState: TodolistType[]
 
-test('correct todolist should be added', ()=>{
-  const todolistId1=v1()
-  const todolistId2=v1()
+beforeEach(() => {
+    startState = [
+      {todolistId: todolistId1, title: 'What to learn', filter: 'all'},
+      {todolistId: todolistId2, title: 'What to buy', filter: 'all'}
+    ]
+  }
+)
 
-  let startState:TodolistType[] = [
-    {todolistId:todolistId1, title:'What to learn', filter:'all'},
-    {todolistId:todolistId2, title:'What to buy', filter:'all'}
-  ]
+test('correct todolist should be added', () => {
 
   const newTodolistTitle = 'New Todolist'
 
-  const endState= todolistsReducer(startState, addTodolistAC(newTodolistTitle))
+  const endState = todolistsReducer(startState, addTodolistAC(newTodolistTitle))
 
   expect(endState.length).toBe(3)
   expect(endState[0].title).toBe('New Todolist')
 
+})
 
+test('correct todolist should be removed', () => {
+  const todolistId1 = v1()
+  const todolistId2 = v1()
+
+  let startState: TodolistType[] = [
+    {todolistId: todolistId1, title: 'What to learn', filter: 'all'},
+    {todolistId: todolistId2, title: 'What to buy', filter: 'all'}
+  ]
+
+  const endState = todolistsReducer(startState, removeTodolistAC(todolistId1))
+
+  expect(endState.length).toBe(1)
+  expect(endState[0].title).toBe('What to buy')
 })
