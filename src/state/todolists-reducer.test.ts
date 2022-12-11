d3d0aa2f3180e208b36cf28dac6f8d8ded7e3674
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-12-11 10:30:23 +0100

    create: add changeTodolistsTitle to todolists-reducer.ts  (8)

--- src/state/todolists-reducer.test.ts
+++ src/state/todolists-reducer.test.ts
@@ -33,3 +33,20 @@ test('correct todolist should be added', () => {
   expect(endState.length).toBe(3)
   expect(endState[0].title).toBe(newTodolistTitle)
 })
+test('correct title todolist should be changed', () => {
+  let todolistId1 = v1()
+  let todolistId2 = v1()
+
+  let newTodolistTitle = 'New Todolist'
+
+  const startState: TodolistsPropsType[] = [
+    {todolistId: todolistId1, title: 'What to learn', filter: 'all'},
+    {todolistId: todolistId2, title: 'What to buy', filter: 'all'}
+  ]
+
+  const endState = todolistsReducer(startState, {type: 'CHANGE-TODOLIST-TITLE', todolistId: todolistId1, title: newTodolistTitle})
+
+  expect(endState.length).toBe(2)
+  expect(endState[0].title).toBe(newTodolistTitle)
+  expect(endState[1].title).toBe('What to buy')
+})
--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -8,6 +8,7 @@ import {v1} from 'uuid';
 export type TodolistsReducerType =
   | RemoveTodolistActionType
   | AddTodolistActionType
+  | ChangeTodolistTitle
 
 type RemoveTodolistActionType = {
   type: 'REMOVE-TODOLIST'
@@ -17,7 +18,11 @@ type AddTodolistActionType = {
   type: 'ADD-TODOLIST'
   title: string
 }
-
+type ChangeTodolistTitle ={
+  type: 'CHANGE-TODOLIST-TITLE'
+  todolistId: string
+  title: string
+}
 
 export const todolistsReducer = (state: TodolistsPropsType[], action: TodolistsReducerType): TodolistsPropsType[] => {
   switch (action.type) {
@@ -27,6 +32,14 @@ export const todolistsReducer = (state: TodolistsPropsType[], action: TodolistsR
       let newTodolistId = v1()
       let newTodolist: TodolistsPropsType = {todolistId: newTodolistId, title: action.title, filter: 'all'};
       return [newTodolist, ...state]
+    }
+       case 'ADD-TODOLIST': {
+      let newTodolistId = v1()
+      let newTodolist: TodolistsPropsType = {todolistId: newTodolistId, title: action.title, filter: 'all'};
+      return [newTodolist, ...state]
+    }
+    case 'CHANGE-TODOLIST-TITLE':{
+      return state.map(tl=>tl.todolistId===action.todolistId?{...tl, title:action.title}:tl)
     }
 
     default:
