abd1c7107a1855c2f1064e5e340238b11fa18026
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-12-11 15:44:20 +0100

    create:add logic for case 'REMOVE-TODOLIST'  (8)

--- src/state/todolists-reducer.ts
+++ src/state/todolists-reducer.ts
@@ -3,9 +3,11 @@ import {v1} from 'uuid';
 
 export type todolistsReducerType=
   | AddTodolistACType
+  | RemoveTodolistACType
 
 
 export type AddTodolistACType = ReturnType<typeof addTodolistAC>
+export type RemoveTodolistACType = ReturnType<typeof removeTodolistAC>
 
 const initialState: TodolistType[] = []
 export const todolistsReducer = (state = initialState, action: todolistsReducerType): TodolistType[] => {
@@ -13,6 +15,8 @@ export const todolistsReducer = (state = initialState, action: todolistsReducerT
     case'ADD-TODOLIST':
       const newTodolistId=v1()
       return [{todolistId: newTodolistId, title: action.payload.title, filter: 'all'}, ...state]
+    case 'REMOVE-TODOLIST':
+      return state.filter(tl=>tl.todolistId!==action.payload.todolistId)
 
     default:
       // throw new Error ('I don\'t understand this type')
@@ -26,5 +30,11 @@ export const addTodolistAC=(title:string)=>{
   return {
     type: 'ADD-TODOLIST',
     payload: {title}
-  }
+  }as const
+}
+export const removeTodolistAC=(todolistId:string)=>{
+  return {
+    type: 'REMOVE-TODOLIST',
+    payload: {todolistId}
+  }as const
 }
