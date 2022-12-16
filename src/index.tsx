8f37155bdfc00bba9342d3df8c5e096fa9349be9
AlesanderZabrodski09 <zabrodski666@gmail.com>
2022-12-16 18:45:58 +0100

    create:add Provider (10)

--- src/index.tsx
+++ src/index.tsx
@@ -6,6 +6,8 @@ import './index.css';
 import { createRoot } from 'react-dom/client';
 // import AppWithReducers from './AppWithReducers';
 import AppWithRedux from './AppWithRedux';
+import {store} from './state/store';
+import {Provider} from 'react-redux';
 
 // const root = ReactDOM.createRoot(
 //   document.getElementById('root') as HTMLElement
@@ -19,7 +21,11 @@ import AppWithRedux from './AppWithRedux';
 
 const container = document.getElementById('root');
 const root = createRoot(container!); // createRoot(container!) if you use TypeScript
-root.render(<AppWithRedux />);
+root.render(
+  <Provider store={store}>
+    <AppWithRedux />
+  </Provider>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
