98f270c83ce94c88b752b9d6a793a784d7107adc
AlesanderZabrodski09 <zabrodski666@gmail.com>
2023-01-18 06:51:31 +0100

    create:add todolist-api.stories.tsx (13)

---
+++ src/stories/todolist-api.stories.tsx
@@ -0,0 +1,38 @@
+import {useEffect, useState} from 'react';
+
+export default {
+  title: 'API'
+}
+
+export const GetTodolists=()=>{
+  const [state, setState]=useState<any>(null)
+  useEffect(()=>{
+
+
+  },[])
+  return <div>{JSON.stringify(state)}</div>
+}
+export const CreateTodolists=()=>{
+  const [state, setState]=useState<any>(null)
+  useEffect(()=>{
+
+
+  },[])
+  return <div>{JSON.stringify(state)}</div>
+}
+export const UpdateTodolists=()=>{
+  const [state, setState]=useState<any>(null)
+  useEffect(()=>{
+
+
+  },[])
+  return <div>{JSON.stringify(state)}</div>
+}
+export const DeleteTodolists=()=>{
+  const [state, setState]=useState<any>(null)
+  useEffect(()=>{
+
+
+  },[])
+  return <div>{JSON.stringify(state)}</div>
+}
