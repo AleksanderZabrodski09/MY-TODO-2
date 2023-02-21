4b3a5b73046810cc0aa60e10744d7aa68ed9922b
AlesanderZabrodski09 <zabrodski666@gmail.com>
2023-02-21 12:51:11 +0100

    create: add app-reducer.ts (15)

---
+++ src/app/app-reducer.ts
@@ -0,0 +1,19 @@
+export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'faild'
+
+const initialState = {
+  status: 'loading'  as RequestStatusType
+}
+
+type InitialStateType = typeof initialState
+
+export const appReducer = (state: InitialStateType = initialState, action: AppActionsType): InitialStateType => {
+  switch (action.type) {
+    case 'APP/SET-STATUS':
+      return {...state, status: action.status}
+    default:
+      return state
+  }
+}
+
+
+type AppActionsType = any
