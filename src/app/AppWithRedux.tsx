32b5132697f49d32799e1dbf0ed4412e9181f887
AlesanderZabrodski09 <zabrodski666@gmail.com>
2023-03-04 06:21:15 +0100

    create: add Routes (16)

--- src/app/AppWithRedux.tsx
+++ src/app/AppWithRedux.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 import './App.css';
 import ButtonAppBar from './AppBar';
 import {Container} from '@mui/material';
-import {TaskType} from '../api/todolist-api';
 import {TodolistsList} from '../features/Todolists/TodolistsList';
 import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar';
-
+import {Navigate, Route, Routes} from 'react-router-dom';
+import {Login} from '../features/Login';
 
 
 function AppWithRedux() {
@@ -14,8 +14,15 @@ function AppWithRedux() {
     <div className="App">
       <ErrorSnackbar/>
       <ButtonAppBar/>
-      <Container>
-        <TodolistsList/>
+      <Container fixed>
+        <Routes>
+          <Route path={'/'} element={<TodolistsList/>}/>
+          <Route path={'/login'} element={<Login/>}/>
+
+          <Route path={'/404'} element={<h1 style={{textAlign: 'center'}}>404: PAGE NOT FOUND</h1>} />
+          <Route path={'*'} element={<Navigate to={'/404'} />} />
+
+        </Routes>
       </Container>
     </div>
   );
